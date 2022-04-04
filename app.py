3d0de8714ce71703f7d1a0a98caa1e6404e29684
Mike Lee <programmablemike@gmail.com>
2022-04-04 14:47:24 -0700

    Removing the ctypes import

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from ctypes import wstring_at
 from api import app
 from ariadne import (load_schema_from_path, make_executable_schema,
                      graphql_sync, snake_case_fallback_resolvers, MutationType, QueryType)
